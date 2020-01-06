5628251154ddea07f9f6aa00d8433b0b219116c0
Randy J. Ray <rjray@blackperl.com>
2020-01-06 13:10:02 -0800

    Added order to author/reference mapping.

--- backend/src/models/authorsreferences.js
+++ backend/src/models/authorsreferences.js
@@ -3,7 +3,12 @@
 module.exports = sequelize => {
   const AuthorsReferences = sequelize.define(
     "AuthorsReferences",
-    {},
+    {
+      order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
     { timestamps: false },
   )
 
