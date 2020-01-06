5dcf3d3cf9fa7394e6053e8ace529cd3a30419db
Randy J. Ray <rjray@blackperl.com>
2020-01-05 21:16:47 -0800

    This relation should be null-able.

--- backend/src/migrations/20200106013242-assoc-reference-magazineissue.js
+++ backend/src/migrations/20200106013242-assoc-reference-magazineissue.js
@@ -2,35 +2,15 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    if (queryInterface.sequelize.getDialect() === "sqlite") {
-      return queryInterface
-        .addColumn("References", "magazineIssueId", {
-          type: Sequelize.INTEGER,
-          references: {
-            model: "MagazineIssues",
-            key: "id",
-          },
-          onUpdate: "CASCADE",
-          onDelete: "CASCADE",
-        })
-        .then(() => {
-          queryInterface.changeColumn("References", "magazineIssueId", {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-          })
-        })
-    } else {
-      return queryInterface.addColumn("References", "magazineIssueId", {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "MagazineIssues",
-          key: "id",
-        },
-        allowNull: false,
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE",
-      })
-    }
+    return queryInterface.addColumn("References", "magazineIssueId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "MagazineIssues",
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    })
   },
 
   down: (queryInterface, Sequelize) => {
