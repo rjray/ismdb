3e51282b7496f2e1b60b1c358241bf061287ec8a
Randy J. Ray <rjray@blackperl.com>
2020-01-10 23:12:16 -0800

    Fix charset for MySQL.

--- backend/src/migrations/00000-fix-db-charset.js
+++ backend/src/migrations/00000-fix-db-charset.js
@@ -5,7 +5,7 @@ module.exports = {
     if (queryInterface.sequelize.getDialect() === "mysql") {
       return queryInterface.sequelize.query(
         `ALTER DATABASE ${queryInterface.sequelize.config.database}
-          CHARACTER SET utf8 COLLATE utf8_general_ci;`
+          CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;`,
       )
     } else {
       return Promise.resolve(null)
