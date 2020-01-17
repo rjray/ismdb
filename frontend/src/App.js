1efe3ed3df0e50b2a4f5668171ce6bc596ce5cb3
Randy J. Ray <rjray@blackperl.com>
2020-01-17 15:53:35 -0800

    Factored out header, injected router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,11 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
-import Button from "react-bootstrap/Button"
-import Navbar from "react-bootstrap/Navbar"
-import Nav from "react-bootstrap/Nav"
-import NavDropdown from "react-bootstrap/NavDropdown"
+
+import Header from "./Header"
 
 const App = () => (
   <Container fluid>
-    <Navbar bg="light" expand="sm">
-      <Navbar.Brand href="#home">My Modeling DB</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown title="Browse" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#references">References</NavDropdown.Item>
-            <NavDropdown.Item href="#authors">Authors</NavDropdown.Item>
-            <NavDropdown.Item href="#magazines">Magazines</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-      <Nav>
-        <Button variant="link">login</Button>
-      </Nav>
-    </Navbar>
+    <Header />
   </Container>
 )
 
