da4f572c056e3fc6f1a556ebc17f38a988811a73
Randy J. Ray <rjray@blackperl.com>
2020-01-18 09:04:26 -0800

    Placeholders for functionality.

---
+++ frontend/src/Magazines.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Magazines = () => {
+  return (
+    <div>
+      <h1>Magazines</h1>
+    </div>
+  )
+}
+
+export default Magazines
---
+++ frontend/src/References.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const References = () => {
+  return (
+    <div>
+      <h1>References</h1>
+    </div>
+  )
+}
+
+export default References
