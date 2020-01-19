97701a33b856941fcb416b654fb2579a7a6608fa
Randy J. Ray <rjray@blackperl.com>
2020-01-19 10:23:06 -0800

    Took out expand code, column tweaking.

--- frontend/src/Magazines.js
+++ frontend/src/Magazines.js
@@ -1,63 +1,11 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
 import ScaleLoader from "react-spinners/ScaleLoader"
 import { formatDistanceToNow } from "date-fns"
 import DataTable from "react-data-table-component"
-import _ from "lodash"
 
 import useDataApi from "./utils/data-api"
-
-const createRow = elements => {
-  if (elements.length < 10) {
-    let id = elements[elements.length - 1].id + 1000
-
-    while (elements.length < 10) {
-      elements.push({ id: id++, number: "" })
-    }
-  }
-
-  return elements.map(item => <Col key={item.id}>{item.number}</Col>)
-}
-
-const ExpandMagazine = props => {
-  const [
-    { data, loading, error },
-  ] = useDataApi(`/api/views/magazine/${props.data.id}`, { data: {} })
-  let content
-
-  if (error) {
-    content = (
-      <>
-        <h3>An Error Occurred</h3>
-        <p>An error occurred trying to load data for magazine:</p>
-        <p>{error.message}</p>
-      </>
-    )
-  } else if (loading) {
-    content = (
-      <div style={{ textAlign: "center" }}>
-        <ScaleLoader />
-      </div>
-    )
-  } else {
-    let magazine = data.magazine
-    let chunks = _.chunk(magazine.MagazineIssues, 10)
-    let rows = chunks.map((row, idx) => {
-      let rowInner = createRow(row)
-      return <Row key={idx}>{rowInner}</Row>
-    })
-
-    content = rows
-  }
-
-  return (
-    <Container style={{ justifyContent: "" }} className="p-2">
-      {content}
-    </Container>
-  )
-}
+import ExpandMagazine from "./ExpandMagazine"
 
 const Magazines = () => {
   const [{ data, loading, error }] = useDataApi("/api/views/magazine/all", {
@@ -83,22 +31,36 @@ const Magazines = () => {
     let magazines = data.magazines
     let columns = [
       {
-        name: "Name",
+        name: <b>Name</b>,
         selector: "name",
         sortable: true,
       },
       {
-        name: "Issues",
+        name: <b>Issues</b>,
         selector: "issues",
         sortable: true,
       },
       {
-        name: "First Added",
+        name: <b>Added</b>,
         selector: "createdAt",
         sortable: true,
-        maxWidth: "25%",
+        maxWidth: "20%",
+        hide: "sm",
         format: row => {
           const now = new Date(row.createdAt)
+          return (
+            <span title={now}>{formatDistanceToNow(new Date(now))} ago</span>
+          )
+        },
+      },
+      {
+        name: <b>Updated</b>,
+        selector: "updatedAt",
+        sortable: true,
+        maxWidth: "20%",
+        hide: "md",
+        format: row => {
+          const now = new Date(row.updatedAt)
           return (
             <span title={now}>{formatDistanceToNow(new Date(now))} ago</span>
           )
@@ -126,11 +88,7 @@ const Magazines = () => {
     )
   }
 
-  return (
-    <>
-      <Container className="mt-2">{content}</Container>
-    </>
-  )
+  return <Container className="mt-2">{content}</Container>
 }
 
 export default Magazines
