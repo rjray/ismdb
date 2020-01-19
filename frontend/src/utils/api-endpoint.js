aa46e67d0055696c7e5b08c14910793729599978
Randy J. Ray <rjray@blackperl.com>
2020-01-18 20:48:45 -0800

    Crib the data-fetcher from MPM.

---
+++ frontend/src/utils/api-endpoint.js
@@ -0,0 +1,9 @@
+/*
+ * As simple/dumb as this is, it keeps the association of apiEndpoint and the
+ * related env variable in one place.
+ */
+
+const apiEndpoint =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : ""
+
+export default apiEndpoint
