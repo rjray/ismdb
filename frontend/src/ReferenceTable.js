673f036aab74fba8b8d768289af548c4ad10df4c
Randy J. Ray <rjray@blackperl.com>
2020-01-20 23:05:58 -0800

    Factored out the ref table.

---
+++ frontend/src/ReferenceTable.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { formatDistanceToNow } from "date-fns"
+import DataTable from "react-data-table-component"
+
+import ReferenceExpand from "./ReferenceExpand"
+
+const ReferenceTable = props => {
+  let columns = [
+    {
+      name: <b>Name</b>,
+      selector: "name",
+      sortable: true,
+      width: "30%",
+    },
+    {
+      name: <b>Source</b>,
+      selector: row => {
+        let str
+
+        if (row.RecordType.description === "book") {
+          str = row.isbn ? `ISBN ${row.isbn}` : "Book"
+        } else if (row.RecordType.description === "article") {
+          str = `${row.Magazine.name} ${row.MagazineIssue.number}`
+        } else if (row.RecordType.description === "placeholder") {
+          str = `${row.Magazine.name} ${row.MagazineIssue.number} (placeholder)`
+        } else {
+          str = row.RecordType.notes
+        }
+
+        return str
+      },
+      width: "25%",
+      sortable: true,
+    },
+    {
+      name: <b>Added</b>,
+      selector: "createdAt",
+      sortable: true,
+      hide: "sm",
+      format: row => {
+        const now = new Date(row.createdAt)
+        return <span title={now}>{formatDistanceToNow(new Date(now))} ago</span>
+      },
+      width: "15%",
+    },
+    {
+      name: <b>Updated</b>,
+      selector: "updatedAt",
+      sortable: true,
+      hide: "md",
+      format: row => {
+        const now = new Date(row.updatedAt)
+        return <span title={now}>{formatDistanceToNow(new Date(now))} ago</span>
+      },
+      width: "15%",
+    },
+  ]
+
+  return (
+    <DataTable
+      striped
+      responsive
+      dense
+      highlightOnHover
+      pointerOnHover
+      pagination
+      paginationPerPage={25}
+      expandableRows
+      expandOnRowClicked
+      expandableRowsComponent={<ReferenceExpand />}
+      defaultSortField="name"
+      columns={columns}
+      {...props}
+    />
+  )
+}
+
+export default ReferenceTable
--- frontend/src/References.js
+++ frontend/src/References.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import ScaleLoader from "react-spinners/ScaleLoader"
-import { formatDistanceToNow } from "date-fns"
-import DataTable from "react-data-table-component"
 
 import useDataApi from "./utils/data-api"
-import ReferenceExpand from "./ReferenceExpand"
+import ReferenceTable from "./ReferenceTable"
 
 const References = () => {
   const [{ data, loading, error }] = useDataApi("/api/views/reference/all", {
@@ -28,79 +28,12 @@ const References = () => {
       </div>
     )
   } else {
-    let references = data.references
-    let columns = [
-      {
-        name: <b>Name</b>,
-        selector: "name",
-        sortable: true,
-        width: "30%",
-      },
-      {
-        name: <b>Source</b>,
-        selector: row => {
-          let str
-
-          if (row.RecordType.description === "book") {
-            str = row.isbn ? `ISBN ${row.isbn}` : "Book"
-          } else if (row.RecordType.description === "article") {
-            str = `${row.Magazine.name} ${row.MagazineIssue.number}`
-          } else if (row.RecordType.description === "placeholder") {
-            str = `${row.Magazine.name} ${row.MagazineIssue.number} (placeholder)`
-          } else {
-            str = row.RecordType.notes
-          }
-
-          return str
-        },
-        width: "25%",
-        sortable: true,
-      },
-      {
-        name: <b>Added</b>,
-        selector: "createdAt",
-        sortable: true,
-        hide: "sm",
-        format: row => {
-          const now = new Date(row.createdAt)
-          return (
-            <span title={now}>{formatDistanceToNow(new Date(now))} ago</span>
-          )
-        },
-        width: "15%",
-      },
-      {
-        name: <b>Updated</b>,
-        selector: "updatedAt",
-        sortable: true,
-        hide: "md",
-        format: row => {
-          const now = new Date(row.updatedAt)
-          return (
-            <span title={now}>{formatDistanceToNow(new Date(now))} ago</span>
-          )
-        },
-        width: "15%",
-      },
-    ]
-
     content = (
-      <DataTable
-        title="References"
-        striped
-        responsive
-        dense
-        highlightOnHover
-        pointerOnHover
-        pagination
-        paginationPerPage={25}
-        expandableRows
-        expandOnRowClicked
-        expandableRowsComponent={<ReferenceExpand />}
-        defaultSortField="name"
-        columns={columns}
-        data={references}
-      />
+      <Row>
+        <Col>
+          <ReferenceTable title="References" data={data.references} />
+        </Col>
+      </Row>
     )
   }
 
