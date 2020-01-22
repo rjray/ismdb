2f2b1c5dfd427ce5467d9673ce4d9cd4186581f1
Randy J. Ray <rjray@blackperl.com>
2020-01-22 11:03:01 -0800

    Extra support for issue-level detail display.

--- backend/src/routes/api/views/magazineissue.js
+++ backend/src/routes/api/views/magazineissue.js
@@ -5,7 +5,13 @@
 const express = require("express")
 const _ = require("lodash")
 
-const { MagazineIssue, Magazine, Reference } = require("../../../models")
+const {
+  MagazineIssue,
+  Magazine,
+  Reference,
+  RecordType,
+  Author,
+} = require("../../../models")
 
 let router = express.Router()
 
@@ -13,13 +19,40 @@ router.get("/:id", (req, res) => {
   let id = req.params.id
   let magazineissue
 
-  MagazineIssue.findByPk(id, { include: [Magazine, Reference] })
+  MagazineIssue.findByPk(id, {
+    include: [
+      Magazine,
+      {
+        model: Reference,
+        include: [
+          RecordType,
+          {
+            model: MagazineIssue,
+            include: [Magazine],
+          },
+          {
+            model: Author,
+            as: "Authors",
+          },
+        ],
+      },
+    ],
+  })
     .then(result => {
       magazineissue = result.get()
 
       magazineissue.Magazine = magazineissue.Magazine.get()
-      magazineissue.References = magazineissue.References.map(item => {
-        return item.get()
+      magazineissue.References = magazineissue.References.map(reference => {
+        reference = reference.get()
+
+        if (reference.MagazineIssue) {
+          reference.Magazine = reference.MagazineIssue.Magazine.get()
+          reference.MagazineIssue = reference.MagazineIssue.get()
+          delete reference.MagazineIssue.Magazine
+        }
+        reference.RecordType = reference.RecordType.get()
+
+        return reference
       })
 
       res.send({ status: "success", magazineissue })
