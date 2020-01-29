7491ae9a801be5268c7cd47eeb59b95328c0cac3
Randy J. Ray <rjray@blackperl.com>
2020-01-29 15:31:39 -0800

    Finished magazine form prototype, extracted it.

---
+++ frontend/src/forms/MagazineForm.js
@@ -0,0 +1,105 @@
+import React from "react"
+import Form from "react-bootstrap/Form"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+
+const MagazineForm = props => {
+  const magazine = props.magazine
+  const created = new Date(magazine.createdAt)
+  const updated = new Date(magazine.updatedAt)
+
+  return (
+    <Form className="mt-3">
+      <Form.Group as={Form.Row} controlId="name">
+        <Form.Label column sm={2} className="text-md-right text-sm-left">
+          Name:
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            type="text"
+            defaultValue={magazine.name}
+            placeholder="Name"
+            data-lpignore="true"
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Form.Row} controlId="language">
+        <Form.Label column sm={2} className="text-md-right text-sm-left">
+          Language:
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            type="text"
+            defaultValue={magazine.language}
+            placeholder="Language"
+            data-lpignore="true"
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Form.Row} controlId="aliases">
+        <Form.Label column sm={2} className="text-md-right text-sm-left">
+          Aliases:
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            type="text"
+            defaultValue={magazine.aliases}
+            placeholder="Aliases"
+            data-lpignore="true"
+          />
+        </Col>
+      </Form.Group>
+      <Form.Group as={Form.Row} controlId="notes">
+        <Form.Label column sm={2} className="text-md-right text-sm-left">
+          Notes:
+        </Form.Label>
+        <Col sm={10}>
+          <Form.Control
+            as="textarea"
+            rows={4}
+            defaultValue={magazine.notes}
+            placeholder="Notes"
+          />
+        </Col>
+      </Form.Group>
+      {magazine.createdAt && (
+        <Form.Group as={Form.Row} controlId="createdAt">
+          <Form.Label column sm={2} className="text-md-right text-sm-left">
+            Created:
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              readOnly
+              plaintext
+              defaultValue={created}
+            />
+          </Col>
+        </Form.Group>
+      )}
+      {magazine.updatedAt && (
+        <Form.Group as={Form.Row} controlId="updatedAt">
+          <Form.Label column sm={2} className="text-md-right text-sm-left">
+            Updated:
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control
+              type="text"
+              readOnly
+              plaintext
+              defaultValue={updated}
+            />
+          </Col>
+        </Form.Group>
+      )}
+      <Form.Group as={Form.Row} className="mt-3">
+        <Col sm={{ span: 10, offset: 2 }}>
+          <Button type="submit">Submit</Button>{" "}
+          <Button type="reset">Reset</Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  )
+}
+
+export default MagazineForm
--- frontend/src/update/MagazineUpdate.js
+++ frontend/src/update/MagazineUpdate.js
@@ -1,12 +1,14 @@
 import React from "react"
+import { LinkContainer } from "react-router-bootstrap"
 import { Helmet } from "react-helmet"
 import Container from "react-bootstrap/Container"
-import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
 import ScaleLoader from "react-spinners/ScaleLoader"
 
 import useDataApi from "../utils/data-api"
+import MagazineForm from "../forms/MagazineForm"
 
 const MagazineUpdate = props => {
   let id = props.match.params.id
@@ -31,56 +33,22 @@ const MagazineUpdate = props => {
       </div>
     )
   } else {
-    let magazine = data.magazine
+    const magazine = data.magazine
 
     content = (
-      <Form>
-        <Form.Group as={Row} controlId="magazine_name">
-          <Form.Label column sm={2} className="text-right">
-            Name
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" value={magazine.name} />
+      <>
+        <Row>
+          <Col>
+            <h2>Magazine Update</h2>
           </Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="magazine_language">
-          <Form.Label column sm={2} className="text-right">
-            Language
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              value={magazine.language}
-              placeholder="Language"
-            />
+          <Col className="text-right">
+            <LinkContainer to={`/magazines/delete/${magazine.id}`}>
+              <Button>Delete</Button>
+            </LinkContainer>
           </Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="magazine_aliases">
-          <Form.Label column sm={2} className="text-right">
-            Aliases
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              value={magazine.aliases}
-              placeholder="Aliases"
-            />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="magazine_notes">
-          <Form.Label column sm={2} className="text-right">
-            Notes
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              as="textarea"
-              rows={4}
-              value={magazine.notes}
-              placeholder="Notes"
-            />
-          </Col>
-        </Form.Group>
-      </Form>
+        </Row>
+        <MagazineForm magazine={magazine} />
+      </>
     )
   }
 
