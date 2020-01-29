7ce327fb1d998cca180f67481f481b73e21ee358
Randy J. Ray <rjray@blackperl.com>
2020-01-29 10:10:57 -0800

    Styling for form placeholders.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "bootstrap/dist/css/bootstrap-reboot.min.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+
+import "./local.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 
