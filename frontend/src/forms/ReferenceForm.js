4b7152bc39fc3b6db08983d101e57d592c852086
Randy J. Ray <rjray@blackperl.com>
2020-02-06 12:42:45 -0800

    Mostly complete, some issues yet to fix.

--- frontend/src/forms/ReferenceForm.js
+++ frontend/src/forms/ReferenceForm.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { LinkContainer } from "react-router-bootstrap"
 import Form from "react-bootstrap/Form"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
@@ -8,6 +8,10 @@ import { Formik, Field, FieldArray, ErrorMessage } from "formik"
 import { MdLink, MdDelete, MdSettingsBackupRestore } from "react-icons/md"
 import _ from "lodash"
 import * as Yup from "yup"
+import ScaleLoader from "react-spinners/ScaleLoader"
+
+import compareVersion from "../utils/compare-version"
+import useDataApi from "../utils/data-api"
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
@@ -68,123 +72,203 @@ const validationSchema = Yup.object().shape({
     ),
 })
 
-const ReferenceRecordType = ({ recordType, isbn, magazine, magazineIssue }) => {
-  const isBook = recordType.description === "book"
-  const isArticle = recordType.description === "article"
-  const isPlaceholder = recordType.description === "placeholder"
+const ReferenceRecordType = () => {
+  const [{ data, loading, error }] = useDataApi("/api/misc/recordtypes", {
+    data: {},
+  })
+  let content
 
-  return (
-    <>
+  if (error) {
+    content = (
+      <>
+        <h3>An Error Occurred</h3>
+        <p>An error occurred trying to load the record types:</p>
+        <p>{error.message}</p>
+      </>
+    )
+  } else if (loading) {
+    content = (
+      <div style={{ textAlign: "center" }}>
+        <ScaleLoader />
+      </div>
+    )
+  } else {
+    const recordtypes = data.recordtypes
+
+    content = (
       <Form.Group as={Form.Row} controlId="recordType" className="mb-2">
         <Form.Label column sm={2} className="text-md-right text-sm-left">
           Record Type:
         </Form.Label>
         <Col sm={10}>
-          <Form.Control
-            type="text"
-            defaultValue={recordType.notes}
-            readOnly
-            plaintext
-          />
+          <Container fluid className="pl-0">
+            <Form.Group as={Form.Row} controlId="isbn" className="mb-2">
+              <Col sm={3}>
+                <Field
+                  as={Form.Control}
+                  type="select"
+                  component="select"
+                  name="RecordTypeId"
+                  style={{ width: "100%" }}
+                >
+                  <option value="">-- Choose --</option>
+                  {recordtypes.map((type, index) => (
+                    <option key={index} value={type.id}>
+                      {type.notes}
+                    </option>
+                  ))}
+                </Field>
+              </Col>
+            </Form.Group>
+          </Container>
         </Col>
       </Form.Group>
-      {isBook && (
-        <Form.Group as={Form.Row} controlId="isbn" className="mb-2">
+    )
+  }
+
+  return content
+}
+
+const ReferenceRecordTypeDetail = ({ recordType, magazine }) => {
+  const isBook = recordType.description === "book"
+  const isArticle = recordType.description === "article"
+  const isPlaceholder = recordType.description === "placeholder"
+  const [{ data, loading, error }] = useDataApi(
+    "/api/views/magazine/namesandissues",
+    {
+      data: {},
+    }
+  )
+  let content
+
+  if (error) {
+    content = (
+      <>
+        <h3>An Error Occurred</h3>
+        <p>An error occurred trying to load the magazine names:</p>
+        <p>{error.message}</p>
+      </>
+    )
+  } else if (loading) {
+    content = (
+      <div style={{ textAlign: "center" }}>
+        <ScaleLoader />
+      </div>
+    )
+  } else {
+    let issues = {}
+    const magazines = data.magazines.map(magazine => {
+      issues[magazine.id] = magazine.MagazineIssues.map(i => i.number).sort(
+        compareVersion
+      )
+      return magazine
+    })
+
+    content = (
+      <>
+        <Form.Group
+          as={Form.Row}
+          controlId="isbn"
+          className="mb-2"
+          style={isBook ? {} : { display: "none" }}
+        >
           <Form.Label column sm={2} className="text-md-right text-sm-left">
             ISBN:
           </Form.Label>
           <Col sm={10}>
-            <Form.Control
-              type="text"
-              defaultValue={isbn}
-              placeholder="Name"
-              data-lpignore="true"
-            />
+            <Container fluid className="pl-0">
+              <Form.Group as={Form.Row} controlId="isbn" className="mb-2">
+                <Col sm={3}>
+                  <Field
+                    as={Form.Control}
+                    type="text"
+                    name="isbn"
+                    placeholder="ISBN"
+                    data-lpignore="true"
+                  />
+                </Col>
+              </Form.Group>
+            </Container>
           </Col>
         </Form.Group>
-      )}
-      {(isArticle || isPlaceholder) && (
-        <Form.Group as={Form.Row} controlId="magazine" className="mb-2">
+        <Form.Group
+          as={Form.Row}
+          controlId="magazine"
+          className="mb-2"
+          style={isArticle || isPlaceholder ? {} : { display: "none" }}
+        >
           <Form.Label column sm={2} className="text-md-right text-sm-left">
             Magazine:
           </Form.Label>
           <Col sm={10}>
-            <Form.Control
-              type="text"
-              defaultValue={`${magazine.name} ${magazineIssue.number}`}
-              readOnly
-              plaintext
-            />
+            <Container fluid className="pl-0">
+              <Form.Group as={Form.Row} controlId="magazine" className="mb-2">
+                <Col sm={3}>
+                  <Field
+                    as={Form.Control}
+                    type="select"
+                    component="select"
+                    name="MagazineId"
+                    style={{ width: "100%" }}
+                  >
+                    <option value="">-- Choose --</option>
+                    {magazines.map((magazine, index) => (
+                      <option key={index} value={magazine.id}>
+                        {magazine.name}
+                      </option>
+                    ))}
+                  </Field>
+                </Col>
+                <Col sm={3}>
+                  <Field
+                    as={Form.Control}
+                    type="text"
+                    name="MagazineIssueNumber"
+                    placeholder="Issue Number"
+                    list="magazine-issues"
+                    data-lpignore="true"
+                  />
+                  <datalist id="magazine-issues">
+                    {magazine &&
+                      magazine.id &&
+                      issues[magazine.id].map((issue, index) => (
+                        <option key={index} value={issue}></option>
+                      ))}
+                  </datalist>
+                </Col>
+              </Form.Group>
+            </Container>
           </Col>
         </Form.Group>
-      )}
-    </>
-  )
-}
+      </>
+    )
+  }
 
-const ReferenceAuthor = ({ author, index }) => {
-  return (
-    <Form.Group as={Form.Row} controlId={`author${index}`} className="mb-2">
-      <Col sm={3}>
-        <Form.Control
-          type="text"
-          defaultValue={author.name}
-          placeholder="Name"
-          data-lpignore="true"
-        />
-      </Col>
-      <Col sm>
-        <span>
-          <Link to={`/authors/${author.id}`} className="text-reset">
-            <MdLink title="Author info" />
-          </Link>{" "}
-          <Button
-            id={`authordelete${index}`}
-            variant="link"
-            className="text-reset"
-          >
-            <MdDelete title="Delete this author" />
-          </Button>
-        </span>
-      </Col>
-    </Form.Group>
-  )
-}
-
-const ReferenceAuthors = ({ reference }) => {
-  const authors = reference.Authors
-
-  return (
-    <Form.Group as={Form.Row} controlId="authors" className="mb-2">
-      <Form.Label column sm={2} className="text-md-right text-sm-left">
-        Authors:
-      </Form.Label>
-      <Col sm={10}>
-        <Container
-          fluid
-          className="mb-2 pb-0 px-0 d-flex flex-column justify-content-start"
-        >
-          {authors.map((author, index) => (
-            <ReferenceAuthor key={index} index={index} author={author} />
-          ))}
-          <Form.Row>
-            <Col>
-              <Button>Add</Button>
-            </Col>
-          </Form.Row>
-        </Container>
-      </Col>
-    </Form.Group>
-  )
+  return content
 }
 
 const ReferenceForm = ({ reference }) => {
   let initialValues = { ...reference }
+  initialValues.authors = _.sortBy(initialValues.Authors, o => o.order).map(
+    item => {
+      return { ...item, delete: false }
+    }
+  )
+  delete initialValues.Authors
   initialValues.createdAt = new Date(initialValues.createdAt)
   initialValues.updatedAt = new Date(initialValues.updatedAt)
+  initialValues.MagazineId = initialValues.Magazine
+    ? initialValues.Magazine.id
+    : ""
+  initialValues.MagazineIssueNumber = initialValues.MagazineIssue
+    ? initialValues.MagazineIssue.number
+    : ""
   delete initialValues.Magazine
   delete initialValues.MagazineIssue
   delete initialValues.RecordType
+  if (!initialValues.isbn) {
+    initialValues.isbn = ""
+  }
 
   return (
     <Formik
@@ -196,6 +280,7 @@ const ReferenceForm = ({ reference }) => {
       }}
     >
       {({
+        values,
         handleChange,
         handleBlur,
         handleSubmit,
@@ -220,13 +305,107 @@ const ReferenceForm = ({ reference }) => {
               />
             </Col>
           </Form.Group>
-          <ReferenceRecordType
+          <ReferenceRecordType />
+          <ReferenceRecordTypeDetail
             recordType={reference.RecordType}
-            isbn={reference.isbn}
             magazine={reference.Magazine}
-            magazineIssue={reference.MagazineIssue}
           />
-          <ReferenceAuthors reference={reference} />
+          <Form.Group as={Form.Row} controlId="aliases" className="mb-2">
+            <Form.Label column sm={2} className="text-md-right text-sm-left">
+              Authors:
+            </Form.Label>
+            <Col sm={10}>
+              <Container
+                fluid
+                className="mb-2 pb-0 px-0 d-flex flex-column justify-content-start"
+              >
+                <FieldArray name="authors">
+                  {helpers => (
+                    <>
+                      {values.authors.map((author, index) => (
+                        <Form.Group
+                          key={index}
+                          as={Form.Row}
+                          controlId={`author${index}`}
+                          className="mb-2"
+                        >
+                          <Col sm={3}>
+                            <Field
+                              as={Form.Control}
+                              type="text"
+                              name={`authors.${index}.name`}
+                              placeholder="Name"
+                              disabled={author.deleted}
+                              className={author.deleted && "strikethrough"}
+                              data-lpignore="true"
+                            />
+                            <ErrorMessage
+                              name={`author.${index}.name`}
+                              component="p"
+                            />
+                          </Col>
+                          <Col sm>
+                            <span>
+                              <Button
+                                variant="link"
+                                className="text-reset"
+                                tabIndex={-1}
+                                onClick={() => {
+                                  if (author.id === 0 && author.name === "") {
+                                    helpers.remove(index)
+                                  } else {
+                                    helpers.replace(index, {
+                                      ...author,
+                                      deleted: !author.deleted,
+                                    })
+                                  }
+                                }}
+                              >
+                                {author.deleted ? (
+                                  <MdSettingsBackupRestore title="Restore this author" />
+                                ) : (
+                                  <MdDelete title="Delete this author" />
+                                )}
+                              </Button>
+                              {author.id ? (
+                                <LinkContainer to={`/authors/${author.id}`}>
+                                  <Button
+                                    variant="link"
+                                    className="text-reset"
+                                    tabIndex={-1}
+                                  >
+                                    <MdLink title="Author info" />
+                                  </Button>
+                                </LinkContainer>
+                              ) : (
+                                ""
+                              )}
+                            </span>
+                          </Col>
+                        </Form.Group>
+                      ))}
+                      <Form.Row>
+                        <Col>
+                          <Button
+                            onClick={() =>
+                              helpers.push({
+                                name: "",
+                                id: 0,
+                                order: 0,
+                                deleted: false,
+                              })
+                            }
+                          >
+                            Add
+                          </Button>
+                        </Col>
+                      </Form.Row>
+                    </>
+                  )}
+                </FieldArray>
+              </Container>
+            </Col>
+          </Form.Group>
           <Form.Group as={Form.Row} controlId="type" className="mb-2">
             <Form.Label column sm={2} className="text-md-right text-sm-left">
               Type:
