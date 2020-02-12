9d95aa8ae4e8f2048bf57e989503e0584e1460b1
Randy J. Ray <rjray@blackperl.com>
2020-02-12 15:31:40 -0800

    Field focus-setting hook. Not sure it works yet.

---
+++ frontend/src/utils/focus.js
@@ -0,0 +1,17 @@
+/*
+ * Simple useFocus hook adapted from
+ * https://stackoverflow.com/questions/28889826/set-focus-on-input-after-render
+ */
+
+import { useRef } from "react"
+
+const useFocus = () => {
+  const htmlElRef = useRef(null)
+  const setFocus = () => {
+    htmlElRef.current && htmlElRef.current.focus()
+  }
+
+  return [htmlElRef, setFocus]
+}
+
+export default useFocus
