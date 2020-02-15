b51106aaf279bd8a903adffa1b9f4f00a4069357
Randy J. Ray <rjray@blackperl.com>
2020-02-15 12:53:15 -0800

    Clarify an error message.

--- frontend/src/update/MagazineUpdate.js
+++ frontend/src/update/MagazineUpdate.js
@@ -24,7 +24,7 @@ const crudHandler = setupCRUDHandler({
     }
   },
   onError: (error, formikBag) => {
-    alert(`Error during update: ${error.message}`)
+    alert(`Error during magazine update: ${error.message}`)
     formikBag.resetForm()
   },
 })
