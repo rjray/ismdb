9292252abf74fb1e489ff85ca0b3285e2f4103d7
Randy J. Ray <rjray@blackperl.com>
2020-08-30 17:24:18 -0700

    Added the languages query in Sequelize form.

--- backend/src/db/misc.js
+++ backend/src/db/misc.js
@@ -2,11 +2,51 @@
  * Miscellaneous queries that don't fit a single theme.
  */
 
-const { RecordType } = require("../models");
+const { Op } = require("sequelize");
+
+const { RecordType, Reference, Sequelize } = require("../models");
 
 // Fetch all RecordType entities.
 const fetchAllRecordTypes = async (opts = {}) => {
   return RecordType.findAll({ ...opts });
 };
 
-module.exports = { fetchAllRecordTypes };
+// Fetch all distinct languages referred to in references.
+const fetchAllLanguages = async (opts = {}) => {
+  const preferred = opts.preferred || "English";
+
+  const results = await Reference.findAll({
+    attributes: [
+      [Sequelize.fn("DISTINCT", Sequelize.col("language")), "language"],
+    ],
+    where: {
+      [Op.and]: [
+        {
+          language: {
+            [Op.ne]: null,
+          },
+        },
+        {
+          language: {
+            [Op.ne]: "",
+          },
+        },
+        {
+          language: {
+            [Op.ne]: preferred,
+          },
+        },
+      ],
+    },
+    order: ["language"],
+  });
+
+  const languages = results.map((l) => l.language);
+  languages.unshift(preferred);
+  return languages;
+};
+
+module.exports = {
+  fetchAllRecordTypes,
+  fetchAllLanguages,
+};
