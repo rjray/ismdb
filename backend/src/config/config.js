3c40c5ffe819fb8eae29082d37e7bf9aadf99df0
Randy J. Ray <rjray@blackperl.com>
2020-09-04 16:54:16 -0700

    Added a "debug" configuration with logging enabled.

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config({
 });
 
 module.exports = {
-  development: {
+  debug: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
@@ -14,6 +14,18 @@ module.exports = {
       collate: "utf8mb4_unicode_ci",
     },
     logging: console.log,
+  },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    define: {
+      charset: "utf8mb4",
+      collate: "utf8mb4_unicode_ci",
+    },
+    logging: false,
   },
   test: {
     username: process.env.DB_USERNAME,
