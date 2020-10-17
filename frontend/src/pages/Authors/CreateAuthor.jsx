cb7685fbe5bc2ce2f953bf993d4ae19d7842fb4a
Randy J. Ray <rjray@blackperl.com>
2020-10-16 21:06:27 -0700

    Fixed a word-ordering; added the multientry toggle.

--- frontend/src/pages/Authors/CreateAuthor.jsx
+++ frontend/src/pages/Authors/CreateAuthor.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import { Helmet } from "react-helmet";
+import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import Header from "../../components/Header";
 import AuthorForm from "../../forms/AuthorForm";
+import { multientrySwitch } from "../../atoms";
 
 const CreateAuthor = () => {
-  const [authorCreated, setAuthorCreated] = useState(false);
+  const [multientry, setMultientry] = useRecoilState(multientrySwitch);
+  const [createdAuthor, setCreatedAuthor] = useState(false);
+
+  const toggleMultientry = () => setMultientry((current) => !current);
 
   const submitHandler = (values, formikBag) => {
     alert(JSON.stringify(values, null, 2));
     formikBag.setSubmitting(false);
   };
 
-  if (authorCreated) {
+  if (createdAuthor && !multientry) {
     return <Redirect push to={{ pathname: "/authors" }} />;
   }
 
@@ -30,10 +36,24 @@ const CreateAuthor = () => {
           <Col>
             <Header>Add an Author</Header>
           </Col>
+          <Col className="text-right">
+            <div className="mr-0 pr-0 align-middle">
+              <span>Enter multiple authors: </span>
+              <Form.Check
+                id="enterMultiple"
+                inline
+                custom
+                type="switch"
+                className="mr-0 pr-0 align-middle"
+                label=""
+                checked={multientry}
+                onChange={toggleMultientry}
+              />
+            </div>
+          </Col>
         </Row>
         <AuthorForm
           submitHandler={submitHandler}
-          action="create"
           author={{ name: "", notes: "", aliases: [] }}
         />
       </Container>
