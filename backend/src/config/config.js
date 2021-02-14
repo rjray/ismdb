41ba6f60ffa9f4d6fda6dc6bd055603be03540ee
Randy J. Ray <rjray@blackperl.com>
2021-02-14 14:12:49 -0800

    Working towards switchable sqlite/mysql dialect.

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,53 +2,71 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
+function getDefines(dialect) {
+  let defines;
+
+  switch (dialect) {
+    case "mysql":
+      defines = {
+        charset: "utf8mb4",
+        collate: "utf8mb4_unicode_ci",
+      };
+      break;
+    default:
+      break;
+  }
+
+  return defines;
+}
+
+const dialect = process.env.DB_DIALECT || "mysql";
+const storage = process.env.DB_STORAGE || "ismdb.db";
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
+const host = process.env.DB_HOST;
+
+const define = getDefines(dialect);
+
 module.exports = {
   debug: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    define: {
-      charset: "utf8mb4",
-      collate: "utf8mb4_unicode_ci",
-    },
+    dialect,
+    username,
+    password,
+    database,
+    host,
+    storage,
+    define,
     logging: console.log,
   },
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    define: {
-      charset: "utf8mb4",
-      collate: "utf8mb4_unicode_ci",
-    },
+    dialect,
+    username,
+    password,
+    database,
+    host,
+    storage,
+    define,
     logging: false,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    define: {
-      charset: "utf8mb4",
-      collate: "utf8mb4_unicode_ci",
-    },
+    dialect,
+    username,
+    password,
+    database,
+    host,
+    storage,
+    define,
     logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    define: {
-      charset: "utf8mb4",
-      collate: "utf8mb4_unicode_ci",
-    },
+    dialect,
+    username,
+    password,
+    database,
+    host,
+    storage,
+    define,
     logging: false,
   },
 };
