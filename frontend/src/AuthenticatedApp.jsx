23827eab3bc57c801dca8bcb6c39ee815eac10ef
Randy J. Ray <rjray@blackperl.com>
2021-03-18 09:59:38 -0700

    Working 401 catching on refresh tokens. Needs more work.

--- frontend/src/AuthenticatedApp.jsx
+++ frontend/src/AuthenticatedApp.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Helmet } from "react-helmet";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ToastProvider } from "react-toast-notifications";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 
+import { useAuth } from "./auth";
 import Home from "./pages/Home";
 import References from "./pages/References";
 import Authors from "./pages/Authors";
@@ -12,35 +13,49 @@ import Magazines from "./pages/Magazines";
 import Issues from "./pages/Issues";
 import Tags from "./pages/Tags";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 24 * 60 * 60 * 1000,
-      cacheTime: 10 * 60 * 1000,
-    },
-  },
-});
+const AuthenticatedApp = () => {
+  const { logout } = useAuth();
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 24 * 60 * 60 * 1000,
+            cacheTime: 10 * 60 * 1000,
+            onError: (error) => {
+              if (error.response.status === 401) {
+                logout();
+              } else {
+                throw error;
+              }
+            },
+          },
+        },
+      }),
+    [logout]
+  );
 
-const AuthenticatedApp = () => (
-  <QueryClientProvider client={queryClient}>
-    <ToastProvider autoDismissTimeout={4000} autoDismiss>
-      <RecoilRoot>
-        <Router>
-          <Helmet titleTemplate="ISMDB - %s">
-            <title>Home</title>
-          </Helmet>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/references" component={References} />
-            <Route path="/authors" component={Authors} />
-            <Route path="/magazines" component={Magazines} />
-            <Route path="/issues" component={Issues} />
-            <Route path="/tags" component={Tags} />
-          </Switch>
-        </Router>
-      </RecoilRoot>
-    </ToastProvider>
-  </QueryClientProvider>
-);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ToastProvider autoDismissTimeout={4000} autoDismiss>
+        <RecoilRoot>
+          <Router>
+            <Helmet titleTemplate="ISMDB - %s">
+              <title>Home</title>
+            </Helmet>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/references" component={References} />
+              <Route path="/authors" component={Authors} />
+              <Route path="/magazines" component={Magazines} />
+              <Route path="/issues" component={Issues} />
+              <Route path="/tags" component={Tags} />
+            </Switch>
+          </Router>
+        </RecoilRoot>
+      </ToastProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default AuthenticatedApp;
