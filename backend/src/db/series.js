988392fe41d23942818f0a57910ca556d5149688
Randy J. Ray <rjray@blackperl.com>
2022-06-06 22:29:19 -0700

    Might need this function other places

--- backend/src/db/series.js
+++ backend/src/db/series.js
@@ -126,6 +126,7 @@ const deleteSeries = async (id) => {
 };
 
 module.exports = {
+  cleanSeriesWithReferences,
   fetchSingleSeriesSimple,
   fetchSingleSeriesComplete,
   fetchAllSeriesSimpleWithCount,
