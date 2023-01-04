6477a6721c9991608c8dc70adcecaea00d72d308
Randy J. Ray <rjray@blackperl.com>
2023-01-03 18:26:16 -0800

    ReferenceTypes and References tables

--- rust-server/migration/src/m20230102_102224_create_reference_type.rs
+++ rust-server/migration/src/m20230102_102224_create_reference_type.rs
@@ -6,43 +6,45 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(ReferenceTypes::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(ReferenceTypes::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(
+                        ColumnDef::new(ReferenceTypes::Name)
+                            .string()
+                            .not_null()
+                            .unique_key(),
+                    )
+                    .col(ColumnDef::new(ReferenceTypes::Description).string())
+                    .col(ColumnDef::new(ReferenceTypes::Notes).string())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(ReferenceTypes::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+enum ReferenceTypes {
+    #[iden = "ReferenceTypes"]
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    Description,
+    Notes,
 }
--- rust-server/migration/src/m20230102_102241_create_reference.rs
+++ rust-server/migration/src/m20230102_102241_create_reference.rs
@@ -1,4 +1,5 @@
 use sea_orm_migration::prelude::*;
+// use common::enums::{ReferenceTypes, References};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,43 +7,75 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(References::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(References::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(References::Name).string().not_null())
+                    .col(ColumnDef::new(References::Language).string())
+                    .col(
+                        ColumnDef::new(References::ReferenceTypeId)
+                            .integer()
+                            .default(Value::Int(None)),
+                    )
+                    .col(
+                        ColumnDef::new(References::CreatedAt)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(References::UpdatedAt)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("FK_reference_referenceType")
+                            .from(
+                                References::Table,
+                                References::ReferenceTypeId,
+                            )
+                            .to(ReferenceTypes::Table, ReferenceTypes::Id),
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(References::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Post {
+enum References {
+    #[iden = "References"]
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    Language,
+    #[iden = "referenceTypeId"]
+    ReferenceTypeId,
+    #[iden = "createdAt"]
+    CreatedAt,
+    #[iden = "updatedAt"]
+    UpdatedAt,
+}
+
+#[derive(Iden)]
+enum ReferenceTypes {
+    #[iden = "ReferenceTypes"]
+    Table,
+    Id,
 }
