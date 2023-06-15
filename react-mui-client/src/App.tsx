69e1179b84c3b0952ad1311b5727324f8d3d83f8
Randy J. Ray <rjray@blackperl.com>
2023-06-15 10:19:56 -0700

    Some small CSS-related tweaks

--- react-mui-client/src/App.tsx
+++ react-mui-client/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,6 +10,7 @@ function App() {
 
   return (
     <>
+      <CssBaseline enableColorScheme />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
